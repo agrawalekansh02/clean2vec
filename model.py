6619527316b4d30a6b417f3cde3cc7c1e55561b3
Ekansh Agrawal <agrawalekansh02@gmil.com>
2022-05-19 21:38:53 -0700

    added embedding into model and added inference

--- model.py
+++ model.py
@@ -1,45 +1,55 @@
 import tensorflow as tf
 
 class Seq2Seq:
-    def __init__(self, input_shape=(27,), output_shape=50, vocab_size=5500, hidden_size=256, dropout_rate=0.5):
-        self.input_shape = input_shape
-        self.output_shape = output_shape
-        self.vocab_size = vocab_size
-        self.hidden_size = hidden_size
-        self.dropout_rate = dropout_rate
-
-    # creates embedding layer, encoders, and decoders
-    def build(self):
-        # embeding
-        # embedding_layer = tf.keras.layers.Embedding(self.vocab_size, 
-        #     output_dim=self.output_shape, 
-        #     input_length = self.input_shape,
-        #     trainable=True,
-        #     name='embedding_layer')
-
-        # encoder layers
-        encoder_input = tf.keras.layers.Input(shape=self.input_shape)
-        # encoder_embedding = embedding_layer(encoder_input)
-        encoder_gru_layer1 = tf.keras.layers.LSTM(self.hidden_size,
+    def __init__(self, embedding_shape=50, vocab_size=5500, hidden_size=256, dropout_rate=0.5):
+        # encoder layer
+        self.encoder_inputs = tf.keras.layers.Input(shape=(None,))
+        self.encoder_embedding = tf.keras.layers.Embedding(vocab_size, 
+            embedding_shape, 
+            mask_zero=True)(self.encoder_inputs)
+        self.encoder_output, self.encoder_hidden, self.encoder_cell = tf.keras.layers.LSTM(hidden_size, 
             return_state=True, 
-            dropout=self.dropout_rate, 
-            name='encoder_gru_1')
-        _, hidden, cell = encoder_gru_layer1(encoder_input)
-        encoder_states = [hidden, cell]
+            dropout=dropout_rate)(self.encoder_embedding)
+        self.encoder_states = [self.encoder_hidden, self.encoder_cell]
 
-        # decoder layers        
-        decoder_input = tf.keras.layers.Input(shape=self.input_shape)
-        # decoder_embedding = embedding_layer(decoder_input)
-        decoder_gru_layer1 = tf.keras.layers.LSTM(self.hidden_size, 
-            return_sequences=True, 
-            return_state=True,
-            dropout=self.dropout_rate,  
-            name='decoder_gru_1')
-        decoder_output, _, _ = decoder_gru_layer1(encoder_input, 
-            initial_state=encoder_states)
-        dense_output = tf.keras.layers.Dense(self.vocab_size, 
-            activation='softmax', 
-            name='decoder_dense')(decoder_output)
+        # decoder layer
+        self.decoder_inputs = tf.keras.layers.Input(shape=(None,))
+        self.decoder_embedding = tf.keras.layers.Embedding(vocab_size, 
+            embedding_shape, 
+            mask_zero=True)(self.decoder_inputs)
+        self.decoder_lstm = tf.keras.layers.LSTM(hidden_size, 
+            return_state=True, 
+            return_sequences=True,
+            dropout=dropout_rate)
+        self.decoder_outputs, _, _ = self.decoder_lstm(self.decoder_embedding,  
+                                initial_state=self.encoder_states)
 
-        model = tf.keras.Model(inputs=[encoder_input, decoder_input], outputs=dense_output)
-        return model
+        # final dense layer
+        self.decoder_dense = tf.keras.layers.Dense(vocab_size, activation='softmax')
+        self.output = self.decoder_dense(self.decoder_outputs)
+
+        self.model = tf.keras.Model(inputs=[self.encoder_inputs, self.decoder_inputs], outputs=self.output)
+
+    def inference(self, embedding_shape=50):
+        # inputs for vectors based on encoder
+        decoder_state_hidden = tf.keras.layer.Input(shape=(embedding_shape,))
+        decoder_state_cell = tf.keras.layer.Input(shape=(embedding_shape,))
+        decoder_states_inputs = [decoder_state_hidden, decoder_state_cell]
+
+        # adding embedded layer as third input
+        decoder_outputs, state_hidden, state_cell = self.decoder_lstm(self.decoder_embedding,
+                                    initial_state=decoder_states_inputs)
+        decoder_states = [state_hidden, state_cell]
+
+        # final dense layer to softmax singular word
+        decoder_outputs = self.decoder_dense(decoder_outputs)
+        decoder_model = tf.keras.Model(inputs=[self.decoder_inputs]+decoder_states_inputs, 
+            outputs=[decoder_outputs]+decoder_states)
+
+        # inital prompt as first input for encoder
+        encoder_model = tf.keras.Model(inputs=self.encoder_inputs,
+            outputs=self.encoder_states)
+
+        return decoder_model, encoder_model
+
+    
