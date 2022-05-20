7d15425556f9b9314ceffaa95a312f97e4f8348d
Ekansh Agrawal <agrawalekansh02@gmil.com>
2022-05-19 21:39:37 -0700

    changed up training script accordingly + 300 epochs and someother gimmicks

--- train.py
+++ train.py
@@ -7,20 +7,21 @@ from model import *
 
 
 VOCAB_SIZE = 5500
-INPUT_SHAPE = (27,VOCAB_SIZE)
-OUTPUT_SHAPE = 50
+INPUT_LEN = 27
+OUTPUT_SHAPE = 100
 HIDDEN_SIZE = 256
 DROPOUT_RATE = 0.5
 
 def train():
     metadata, idx_q, idx_a = load_data()
-    (trainX, trainY), (testX, testY), (validX, validY) = split_dataset(idx_q, idx_a)
+    (trainX, trainY), (testX, testY) = split_dataset(idx_q, idx_a)
 
-    _model = Seq2Seq(INPUT_SHAPE, OUTPUT_SHAPE, VOCAB_SIZE, HIDDEN_SIZE, DROPOUT_RATE)
-    model = _model.build()
+    _model = Seq2Seq(OUTPUT_SHAPE, VOCAB_SIZE, HIDDEN_SIZE, DROPOUT_RATE)
+    model = _model.model
     model.compile(loss='categorical_crossentropy', 
         optimizer='adam', 
         metrics=['accuracy'])
+    print(model.summary())
 
     trainX1 = trainX.copy()
     trainX2 = trainY.copy()
@@ -28,11 +29,13 @@ def train():
         np.roll(trainY[i,:], -1)
         trainY[i,-1] = 0
 
-    trainX1 = tf.keras.utils.to_categorical(trainX1, VOCAB_SIZE)
-    trainX2 = tf.keras.utils.to_categorical(trainX2, VOCAB_SIZE)
+    print(trainX1.shape)
+    print(trainY.shape)
+
     trainY = tf.keras.utils.to_categorical(trainY, VOCAB_SIZE)
 
-    model.fit([trainX1, trainX2], trainY, batch_size=64, epochs=100, validation_split=0.2)
+    model.fit([trainX1, trainX2], trainY, batch_size=64, epochs=300, validation_split=0.2)
+    model.save('data/model.h5')
 
 
 if __name__ == '__main__':
